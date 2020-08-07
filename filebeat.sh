690f4e07b0b073c55517238eee56fc5eed7f3c56
besha100 <36448614+besha100@users.noreply.github.com>
2020-08-07 20:13:46 +0200

    Update filebeat.sh

--- filebeat.sh
+++ filebeat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a 
 /etc/apt/sources.list.d/elastic-6.x.list
-sudo apt-get update && sudo apt-get install filebeat
+sudo apt-get update -y && sudo apt-get install filebeat -y
 sudo rm -f /etc/filebeat/filebeat.yml
 sudo cp filebeat.yml /etc/filebeat/ && sudo cp suricata /etc/logrotate.d/ && sudo logrotate /etc/logrotate.d/suricata && sudo chmod 775 /var/log/suricata/eve.json 
